46ee5fe0e643be10c518ec964009cc65c397b527
kshitijom <mishrakshitij.646@gmail.com>
2020-09-18 11:37:35 +0530

    Add color helper to generate colors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelper';
 import Palette from './Palette';
 
 class App extends Component {
-  render () {
-    return (
-      <div className="App">
-        <Palette {...seedColors[4]}/>
-      </div>
-    );
-  }
+	render() {
+    console.log(generatePalette(seedColors[4]));
+		return (
+			<div className="App">
+				<Palette {...seedColors[4]} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -6,27 +6,24 @@ export class ColorBox extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { copied: false };
-    this.changeCopyState = this.changeCopyState.bind(this);
-  }
+		this.changeCopyState = this.changeCopyState.bind(this);
+	}
 	changeCopyState() {
 		this.setState({ copied: true }, () => {
 			setTimeout(() => this.setState({ copied: false }), 1500);
 		});
 	}
 	render() {
-    const { name, background } = this.props;
-    const {copied} = this.state;
+		const { name, background } = this.props;
+		const { copied } = this.state;
 		return (
 			<CopyToClipboard text={background} onCopy={this.changeCopyState}>
 				<div style={{ background }} className="ColorBox">
-					<div 
-            style={{ background }} 
-            className={`copy-overlay ${copied && 'show'}`} 
-          />
-          <div className={`copy-msg ${copied && 'show'}`}>
-            <h1>COPIED!</h1>
-            <p>{this.props.background}</p>
-          </div>
+					<div style={{ background }} className={`copy-overlay ${copied && 'show'}`} />
+					<div className={`copy-msg ${copied && 'show'}`}>
+						<h1>COPIED!</h1>
+						<p>{this.props.background}</p>
+					</div>
 					<div className="copy-container">
 						<div className="box-content">
 							<span>{name}</span>
